8b8c95e63ed372112e458371fe2be3729301c7c1
Jerome Chery <jerome.chery@nrco.fr>
2018-10-27 21:32:15 +0200

    Electron

---
+++ links.js
@@ -0,0 +1,52 @@
+// eslint-disable-next-line
+links = {
+	Vue: "https://vuejs.org/v2/guide/",
+
+	AceEditor: [
+		"https://ace.c9.io/",
+		"https://github.com/thlorenz/brace",
+		"https://github.com/sourcerer-io/electron-vue-buefy-editor/blob/master/src/renderer/components/EditorPage.vue",
+	],
+
+	PhotonKit: "http://photonkit.com/components/",
+	VuePhotonKit: require('vue-photonkit'),
+
+	JsonEditor: [
+		"https://jinkin1995.github.io/vue-json-edit/",
+		"https://github.com/fluency03/vue-json-form",
+		"http://www.vuetoolbox.com/projects/vue-json-tree-view",
+	],
+
+	Tree: [
+		"http://www.vuetoolbox.com/projects/vue-tree",
+		"https://itnext.io/a-file-system-browser-component-for-electron-vue-js-applications-eab285fbb3cb",
+		"https://fr.vuejs.org/v2/examples/tree-view.html",
+	],
+	walk: require('walkdir'),
+
+	Upload: "https://www.vuescript.com/vue-js-base64-file-upload/",
+
+	Settings: [
+		"https://www.npmjs.com/package/electron-config",
+		"https://www.npmjs.com/package/electron-settings",
+	],
+
+	Search: [
+		"https://ace.c9.io/#nav=api&api=search",
+		"https://jsfiddle.net/_kemar/3rg6a09b/",
+	],
+
+	NumPag: [
+		"https://github.com/dompdf/dompdf/issues/571",
+		"https://stackoverflow.com/questions/20050939/print-page-numbers-on-pages-when-printing-html",
+	],
+
+	Find: [
+		"https://www.npmjs.com/package/find-in-files",
+		"https://www.npmjs.com/package/replace",
+	],
+
+	Markdown: [
+		"https://github.com/evbacher/gd2md-html/wiki",
+	]
+}
