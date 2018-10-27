700231bcfba5b03bd29392247705e624fc1c1f59
Jerome Chery <jerome.chery@nrco.fr>
2018-10-27 22:18:29 +0200

    photon

---
+++ src/components/MainWindow.vue
@@ -0,0 +1,44 @@
+<template>
+  <ph-window>
+    <ph-toolbar type="header">
+      <ph-toolbar-actions>
+        <ph-button-group class="pull-left">
+          <ph-button size="sm">
+            <ph-icon icon="folder"></ph-icon>
+          </ph-button>
+          <ph-button size="sm">
+            <ph-icon icon="quote"></ph-icon>
+          </ph-button>
+          <ph-button size="sm">
+            <ph-icon icon="tools"></ph-icon>
+          </ph-button>
+        </ph-button-group>
+      </ph-toolbar-actions>
+    </ph-toolbar>
+    <ph-window-content>
+      <ph-pane-group>
+        <ph-pane size="sm" :sidebar="true" style="overflow-y:auto;">
+        </ph-pane>
+        <ph-pane>
+          <ph-tab-group>
+            <ph-tab-item/>
+          </ph-tab-group>
+        </ph-pane>
+      </ph-pane-group>
+    </ph-window-content>
+    <ph-toolbar type="footer"></ph-toolbar>
+  </ph-window>
+</template>
+
+<script>
+export default {
+  name: 'MainWindow',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import '../node_modules/photonkit/dist/css/photon.css'
+import install from 'vue-photonkit'
 
+Vue.use(install)
 Vue.config.productionTip = false
 
 new Vue({
